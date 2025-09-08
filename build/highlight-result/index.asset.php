de03e0e164aa354056a4551a14d8fb0b03768153
Raphael Schär <raphaelschaer81@gmail.com>
2025-09-08 16:03:23 +0200

    build the src

--- build/highlight-result/index.asset.php
+++ build/highlight-result/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c33ae8a12598ecf2c15d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7d238d735ed64523c02b');
--- build/rankings/index.asset.php
+++ build/rankings/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f20f6f20cb1d60936da8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4a1104384f538cdbb399');
